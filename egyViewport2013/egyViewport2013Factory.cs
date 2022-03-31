591b2090688e54f666122b9d14cffefa827bb517
Rico <RicoP@users.noreply.github.com>
2022-04-01 00:51:51 +0200

    make it compile and build for VS2019

--- egyViewport2013/egyViewport2013Factory.cs
+++ egyViewport2013/egyViewport2013Factory.cs
@@ -1,6 +1,33 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
 using Microsoft.VisualStudio.Utilities;
+
+public static class Logger {
+    static StreamWriter log = new StreamWriter(System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\blablub.log");
+
+    public static void Write(string str) {
+        log.Write(DateTime.Now.ToShortTimeString());
+        log.Write(" >");
+        log.WriteLine(str);
+        log.Flush();
+    }
+
+    public static string GetPath(this IWpfTextView textView) {
+        textView.TextBuffer.Properties.TryGetProperty(typeof(IVsTextBuffer), out IVsTextBuffer bufferAdapter);
+
+        if (!(bufferAdapter is IPersistFileFormat persistFileFormat)) {
+            return null;
+        }
+        persistFileFormat.GetCurFile(out string filePath, out _);
+        return filePath;
+    }
+
+}
+
 
 namespace egyViewport2013
 {
@@ -12,7 +39,7 @@ namespace egyViewport2013
     [Export(typeof(IWpfTextViewCreationListener))]
     [ContentType("text")]
     [TextViewRole(PredefinedTextViewRoles.Document)]
-    internal sealed class PurpleBoxAdornmentFactory : IWpfTextViewCreationListener
+    public sealed class PurpleBoxAdornmentFactory : IWpfTextViewCreationListener
     {
         /// <summary>
         /// Defines the adornment layer for the scarlet adornment. This layer is ordered 
@@ -22,13 +49,16 @@ namespace egyViewport2013
         [Name("egyViewport2013")]
         [Order(After = PredefinedAdornmentLayers.Caret)]
         public AdornmentLayerDefinition editorAdornmentLayer = null;
-
+        
+        
         /// <summary>
         /// Instantiates a egyViewport2013 manager when a textView is created.
         /// </summary>
         /// <param name="textView">The <see cref="IWpfTextView"/> upon which the adornment should be placed</param>
         public void TextViewCreated(IWpfTextView textView)
         {
+            Logger.Write("TextView Created");
+
             new egyViewport2013(textView);
         }
     }
