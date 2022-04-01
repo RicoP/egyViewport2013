38bf89719d69fc2d8e210ef2af0e811be8ab6f0a
Rico <RicoP@users.noreply.github.com>
2022-04-01 13:59:24 +0200

    refactoring. dispose log file on close

--- egyViewport2013/egyViewport2013.cs
+++ egyViewport2013/egyViewport2013.cs
@@ -22,6 +22,7 @@ namespace egyViewport2013 {
     /// </summary>
     class egyViewport2013 {
         private Image _image;
+        private BitmapImage _bitmap;
         private IWpfTextView _view;
         private IAdornmentLayer _adornmentLayer;
 
@@ -31,12 +32,8 @@ namespace egyViewport2013 {
         /// <param name="view">The <see cref="IWpfTextView"/> upon which the adornment will be drawn</param>
         public egyViewport2013(IWpfTextView view) {
 
-            string location = Helper.GetPath(view);
-            Helper.Write("TextView Created in location " + (location ?? "UNKNOWN"));
-
-            _view = view;
-
             var fileNameConfig = "config.json";
+            var location = Helper.GetPath(view) ?? "UNKNOWN";
             var assemblyLoc = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             var config_path = Path.Combine(assemblyLoc, fileNameConfig);
             var vConfig = LoadConfigFile(config_path);
@@ -44,17 +41,17 @@ namespace egyViewport2013 {
             var fallback_image_path = Path.Combine(assemblyLoc, image_name);
             var imageOpacity = vConfig.imageOpacity;
             var image_path = GetParentImagePath(location) ?? fallback_image_path;
-            var bitmapImage = LoadBitmap(image_path);
-            var image = CreateImage(bitmapImage, imageOpacity);
-
-            _image = image;
+            SetupImageAndBitmap(image_path, imageOpacity);
 
             //Grab a reference to the adornment layer that this adornment should be added to
-            _adornmentLayer = view.GetAdornmentLayer("egyViewport2013");
-
-            this.onSizeChange();
+            _view = view;
             _view.ViewportHeightChanged += delegate { this.onSizeChange(); };
             _view.ViewportWidthChanged += delegate { this.onSizeChange(); };
+            
+            _adornmentLayer = view.GetAdornmentLayer("egyViewport2013");
+
+            Helper.Write($"TextView Created in location {location}.");
+            onSizeChange();
         }
 
         private string GetParentImagePath(string location) {
@@ -69,26 +66,27 @@ namespace egyViewport2013 {
             }
         }
 
-        private static Image CreateImage(BitmapImage bitmapImage, double imageOpacity) {
+        private void SetupImageAndBitmap(string image_path, double imageOpacity) {
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri(image_path, UriKind.Absolute);
+            bitmapImage.EndInit();
+
             var image = new Image();
             image.MaxWidth = bitmapImage.PixelWidth;
             image.MaxHeight = bitmapImage.PixelHeight;
             image.Opacity = imageOpacity;
             image.Source = bitmapImage;
-            image.Stretch = Stretch.Uniform;
-            return image;
-        }
+            //image.Stretch = Stretch.Uniform;
+            image.Stretch = Stretch.None;
 
-        private static BitmapImage LoadBitmap(string image_path) {
-            BitmapImage bitmapImage = new BitmapImage();
-            bitmapImage.BeginInit();
-            bitmapImage.UriSource = new Uri(image_path, UriKind.Absolute);
-            bitmapImage.EndInit();
-            return bitmapImage;
+            _image = image;
+            _bitmap = bitmapImage;
+
+            Helper.Write($"Loading Image {image_path}, PW{bitmapImage.PixelWidth}, PH{bitmapImage.PixelHeight}, Opacity{imageOpacity}");
         }
 
         private static egyConfig LoadConfigFile(string config_path) {
-            egyConfig vConfig;
             FileStream fs = new FileStream(config_path, FileMode.Open);
             Stream mStream = new MemoryStream();
             DataContractJsonSerializer dcjs = new DataContractJsonSerializer(typeof(egyConfig));
@@ -98,24 +96,33 @@ namespace egyViewport2013 {
             mStream.Write(fByte, 0, (int)fLength);
             fs.Dispose();
             mStream.Position = 0;
-            vConfig = (egyConfig)dcjs.ReadObject(mStream);
+            var vConfig = (egyConfig)dcjs.ReadObject(mStream);
             mStream.Dispose();
             return vConfig;
         }
 
         public void onSizeChange() {
+            double zoom = _view.ZoomLevel;
+            Helper.Write($"onSizeChange VW{_view.ViewportWidth}, VH{_view.ViewportHeight}, IW{_image.Width}, IH{_image.Height}");
+
             //clear the adornment layer of previous adornments
             _adornmentLayer.RemoveAllAdornments();
 
             //stretch image size
-            double width = _view.ViewportWidth;
-            double height = _view.ViewportHeight;
-            _image.Width = width;
-            _image.Height = height;
+            //_image.Width = _view.ViewportWidth;
+            //_image.Height = _view.ViewportHeight;
+            //_image.Width = _bitmap.PixelWidth / zoom;
+            //_image.Height = _bitmap.PixelHeight / zoom;
+            //var width = _image.Source.Width / zoom;
+            //var height = _image.Source.Height / zoom;
+            var width = _bitmap.PixelWidth;
+            var height = _bitmap.PixelHeight;
+            //var width = _image.Width;
+            //var height = _image.Height;
 
             //Place the image in the right hand of the Viewport
-            Canvas.SetLeft(_image, _view.ViewportRight - _image.Width);
-            Canvas.SetTop(_image, _view.ViewportBottom - _image.Height);
+            Canvas.SetLeft(_image, _view.ViewportRight - width);
+            Canvas.SetTop(_image, _view.ViewportBottom - height);
 
             //add the image to the adornment layer and make it relative to the viewport
             _adornmentLayer.AddAdornment(AdornmentPositioningBehavior.ViewportRelative, null, null, _image, null);
--- egyViewport2013/egyViewport2013Factory.cs
+++ egyViewport2013/egyViewport2013Factory.cs
@@ -7,16 +7,17 @@ using Microsoft.VisualStudio.TextManager.Interop;
 using Microsoft.VisualStudio.Utilities;
 
 public static class Helper {
-    static StreamWriter log = new StreamWriter(System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\blablub.log");
+    static readonly string path = $"{System.IO.Path.GetTempFileName()}.log.imagevs";
+    static StreamWriter log = new StreamWriter(path);
 
-    public static void Write(string str) {
+    static public void Write(string str) {
         log.Write(DateTime.Now.ToShortTimeString());
         log.Write(" >");
         log.WriteLine(str);
         log.Flush();
     }
 
-    public static string GetPath(IWpfTextView textView) {
+    static public string GetPath(IWpfTextView textView) {
         textView.TextBuffer.Properties.TryGetProperty(typeof(IVsTextBuffer), out IVsTextBuffer bufferAdapter);
 
         if (!(bufferAdapter is IPersistFileFormat persistFileFormat)) {
@@ -25,10 +26,14 @@ public static class Helper {
         persistFileFormat.GetCurFile(out string filePath, out _);
         return filePath;
     }
+
+    static public void Kill() {
+        log.Close();
+        File.Delete(path);
+    }
 }
 
-namespace egyViewport2013
-{
+namespace egyViewport2013 {
     #region Adornment Factory
     /// <summary>
     /// Establishes an <see cref="IAdornmentLayer"/> to place the adornment on and exports the <see cref="IWpfTextViewCreationListener"/>
@@ -37,8 +42,7 @@ namespace egyViewport2013
     [Export(typeof(IWpfTextViewCreationListener))]
     [ContentType("text")]
     [TextViewRole(PredefinedTextViewRoles.Document)]
-    public sealed class PurpleBoxAdornmentFactory : IWpfTextViewCreationListener
-    {
+    public sealed class PurpleBoxAdornmentFactory : IWpfTextViewCreationListener, IDisposable {
         /// <summary>
         /// Defines the adornment layer for the scarlet adornment. This layer is ordered 
         /// after the selection layer in the Z-order
@@ -47,14 +51,16 @@ namespace egyViewport2013
         [Name("egyViewport2013")]
         [Order(After = PredefinedAdornmentLayers.Caret)]
         public AdornmentLayerDefinition editorAdornmentLayer = null;
-        
-        
+
+        public void Dispose() {
+            Helper.Kill();
+        }
+
         /// <summary>
         /// Instantiates a egyViewport2013 manager when a textView is created.
         /// </summary>
         /// <param name="textView">The <see cref="IWpfTextView"/> upon which the adornment should be placed</param>
-        public void TextViewCreated(IWpfTextView textView)
-        {
+        public void TextViewCreated(IWpfTextView textView) {
             new egyViewport2013(textView);
         }
     }
